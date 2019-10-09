aa1079b83249b821b691ff6ed7ff32d7ff575c9b
Kenta IDA <fuga@fugafuga.org>
2019-10-09 15:28:32 +0900

    Rename log CSV files Add log analyze script

--- log/hwtimer.mk
+++ log/hwtimer.mk
@@ -1,12 +1,17 @@
+CSV_UDP    ?= udp.csv
+CSV_NOUDP  ?= noudp.csv
+CSV_APSCAN ?= apscan.csv
+CSV_NOWIFI ?= nowifi.csv
+
 CSV_ALL := $(CSV_UDP) $(CSV_NOUDP) $(CSV_APSCAN) $(CSV_NOWIFI)
 FORMAT := pngcairo
 EXT := png
 TARGETS := hwtimer_interval.$(EXT) hwtimer_delay.$(EXT)
 
-all: $(TARGETS)
+all: $(TARGETS) intervals.txt delays.txt
 
 clean:
-	-@$(RM) $(TARGETS)
+	-@$(RM) $(TARGETS) intervals.txt delays.txt
 
 hwtimer_interval.$(EXT): $(CSV_ALL)
 	gnuplot -e "set terminal $(FORMAT) mono size 800,600;set output \"$@\";set yrange [0:1000]" -c ../../tool/plot_states.plt \
@@ -21,3 +26,17 @@ hwtimer_delay.$(EXT): $(CSV_ALL)
 		$(CSV_NOUDP)  "hwtimer (noudp)" \
 		$(CSV_APSCAN) "hwtimer (apscan)" \
 		$(CSV_NOWIFI) "hwtimer (nowifi)"
+
+intervals.txt: $(CSV_ALL)
+	-$(RM) $@
+	python3 ../../tool/loganalyze.py ${CSV_UDP}    2 udp    >> $(abspath $@)
+	python3 ../../tool/loganalyze.py ${CSV_NOUDP}  2 noudp  >> $(abspath $@)
+	python3 ../../tool/loganalyze.py ${CSV_APSCAN} 2 apscan >> $(abspath $@)
+	python3 ../../tool/loganalyze.py ${CSV_NOWIFI} 2 nowifi >> $(abspath $@)
+
+delays.txt: $(CSV_ALL)
+	-$(RM) $@
+	python3 ../../tool/loganalyze.py ${CSV_UDP}    1 udp    >> $(abspath $@)
+	python3 ../../tool/loganalyze.py ${CSV_NOUDP}  1 noudp  >> $(abspath $@)
+	python3 ../../tool/loganalyze.py ${CSV_APSCAN} 1 apscan >> $(abspath $@)
+	python3 ../../tool/loganalyze.py ${CSV_NOWIFI} 1 nowifi >> $(abspath $@)
